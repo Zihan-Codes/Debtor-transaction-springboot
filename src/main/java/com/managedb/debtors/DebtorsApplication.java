a66e5db912c1fda9482ca78b13e815bb9997c5fe
ZihanZHN <zim.zimti1@gmail.com>
2023-03-05 16:41:54 +0530

    first commit

---
+++ src/main/java/com/managedb/debtors/DebtorsApplication.java
@@ -0,0 +1,13 @@
+package com.managedb.debtors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebtorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/managedb/debtors/DebtorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managedb.debtors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
