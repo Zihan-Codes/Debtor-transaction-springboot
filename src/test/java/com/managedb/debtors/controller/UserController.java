5fead012ce7e33e8ce903d0723aeed716d7340f9
ZihanZHN <zim.zimti1@gmail.com>
2023-03-07 07:49:09 +0530

    Created the UserRepository and UserController

---
+++ src/test/java/com/managedb/debtors/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.managedb.debtors.controller;
+
+import com.managedb.debtors.model.User;
+import com.managedb.debtors.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
